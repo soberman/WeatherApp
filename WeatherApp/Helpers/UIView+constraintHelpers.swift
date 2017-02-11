64b3c492e5392fa2ea81e2bb3df93afd6bfc1868
Yarik Arsenkin <majesticy@gmail.com>
2017-02-11 23:32:29 +0100

    Adds image for the settings button.

---
+++ WeatherApp/Helpers/UIView+constraintHelpers.swift
@@ -0,0 +1,29 @@
+//
+//  UILayoutAnchor+Helpers.swift
+//  WeatherApp
+//
+//  Created by Yaroslav Arsenkin on 11/02/2017.
+//  Copyright © 2017 Yarik Arsenkin. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+	
+	func wh_stickAllEdges(to object: UIView, margin: CGFloat = 0) {
+		self.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			self.leftAnchor.constraint(equalTo: object.leftAnchor, constant: margin),
+			self.topAnchor.constraint(equalTo: object.topAnchor, constant: margin),
+			self.rightAnchor.constraint(equalTo: object.rightAnchor, constant: -margin),
+			self.bottomAnchor.constraint(equalTo: object.bottomAnchor, constant: -margin)
+		])
+	}
+	
+	func wh_activateConstraints(_ constraints: [NSLayoutConstraint]) {
+		self.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate(constraints)
+	}
+	
+	
+}
